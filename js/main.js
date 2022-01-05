2255a310fcfe0bda5143386cef966895cd13caab
Elijah <eligenius@gmail.com>
2022-01-05 10:41:42 +0100

    Update UI Class by including: "add book method" "delete book method"

--- js/main.js
+++ js/main.js
@@ -1,42 +1,4 @@
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const inserBooktBtn = document.getElementById('insertBtn');
 const listOutput = document.getElementById('lsOutput');
-
-const booklist = JSON.parse(localStorage.getItem('Books')) || [];
-
-inserBooktBtn.onclick = () => {
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
-  if (title === '' || author === '') {
-    alert('Inputs cannot be empty');
-  } else {
-    const newBook = {
-      title,
-      author,
-    };
-    booklist.unshift(newBook);
-    localStorage.setItem('Books', JSON.stringify(booklist));
-    location.reload();
-  }
-};
-
-function removeBook(val) {
-  const book = booklist.filter((data, i) => i !== val);
-  localStorage.setItem('Books', JSON.stringify(book));
-  location.reload();
-}
-
-for (let i = 0; i < booklist.length; i += 1) {
-  const book = booklist[i];
-  listOutput.innerHTML += `<div id="addBook" class="addBook">
-                              <span">${'  '}</span>
-                              <span class="book-info item1">"${book.title}"</span></br>
-                              <span class="book-info item2">${book.author}</span></br>
-                              <input type="submit" class="bttn" value="Remove" id="delBtn" onclick="removeBook(${i})">
-                              <hr>
-                          </div>`;
-}
 
 class Store {
   static getBooks() {
@@ -73,6 +35,49 @@ class UI {
     const bookList = Store.getBooks();
     bookList.forEach((book) => UI.addBookToList(book));
   }
+
+  static addBookToList(book) {
+    listOutput.innerHTML += `<div class="book" id="${book.id}">
+                             <div class="book-infos">
+                                <span>${book.title}</span>  <span>by</span>
+                                <span>${book.author}</span>
+                                <button class="removeBtn" >Delete</button>
+                              </div>
+                            </div>
+                            `;
+  }
+  static deleteBook(id) {
+    const el = document.getElementById(`${id}`);
+    el.parentNode.removeChild(el);
+  }
 }
 UI.displayBook();
 
+//--------------------------------------------------------------- 
+class Book {
+  constructor(title, author) {
+    this.id = new Date().valueOf();
+    this.title = title;
+    this.author = author;
+  }
+}
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (e.target.title.value === '' || e.target.author.value === '') {
+    alert('Fields not supposed to be empty!');
+  } else {
+    const author = e.target.author.value.trim();
+    const title = e.target.title.value.trim();
+    const newBook = new Book(author, title);
+    Store.addBook(newBook);
+    UI.addBookToList(newBook);
+    e.target.reset();
+  }
+});
+listOutput.addEventListener('click', (e) => {
+  UI.deleteBook(e.target.parentElement.id);
+  Store.removeBook(e.target.parentElement.id);
+});
+UI.displayBook();
+//---------------------------------------------------------------
