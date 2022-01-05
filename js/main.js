98d451a64bed01bb4656a0c3acfa6e5459e087e7
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-05 11:51:00 +0200

    Revert "Update js/main.js >> Add click event on Delete-Book botton"
    This reverts commit e2adb2a6936acba0bbb427f5d6396a2c3ad35a27.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,28 @@
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const inserBooktBtn = document.getElementById('insertBtn');
 const listOutput = document.getElementById('lsOutput');
 
-const booklist = JSON.parse(localStorage.getItem('Books')) || [];
-
-inserBooktBtn.onclick = () => {
-  const title = inputTitle.value;
-  const author = inputAuthor.value;
-  if (title === '' || author === '') {
-    alert('Inputs cannot be empty');
-  } else {
-    const newBook = {
-      title,
-      author,
-    };
-    booklist.unshift(newBook);
-    localStorage.setItem('Books', JSON.stringify(booklist));
-    location.reload();
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('Books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('Books'));
+    }
+    return books;
   }
-};
-
-function removeBook(val) {
-  const book = booklist.filter((data, i) => i !== val);
-  localStorage.setItem('Books', JSON.stringify(book));
-  location.reload();
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('Books', JSON.stringify(books));
+  }
+  static removeBook(id) {
+    let books = Store.getBooks();
+    const updatedBooks = books.filter((book) => book.id !== parseInt(id, 10));
+    books = updatedBooks;
+    localStorage.setItem('Books', JSON.stringify(books));
+  }
 }
 
-for (let i = 0; i < booklist.length; i += 1) {
-  const book = booklist[i];
-  listOutput.innerHTML += `<div id="addBook" class="addBook">
-                              <span">${'  '}</span>
-                              <span class="book-info item1">"${book.title}"</span></br>
-                              <span class="book-info item2">${book.author}</span></br>
-                              <input type="submit" class="bttn" value="Remove" id="delBtn" onclick="removeBook(${i})">
-                              <hr>
-                          </div>`;
-}
-
-class Store {}
 class Book {
   constructor(title, author) {
     this.id = new Date().valueOf();
@@ -51,11 +35,20 @@ class UI {
     const bookList = Store.getBooks();
     bookList.forEach((book) => UI.addBookToList(book));
   }
+
+  static addBookToList(book) {
+    listOutput.innerHTML += `<div class="book" id="${book.id}">
+                             <div class="book-infos">
+                                <span>${book.title}</span>  <span>by</span>
+                                <span>${book.author}</span>
+                                <button class="removeBtn" >Delete</button>
+                              </div>
+                            </div>
+                            `;
+  }
+  static deleteBook(id) {
+    const el = document.getElementById(`${id}`);
+    el.parentNode.removeChild(el);
+  }
 }
-
-listOutput.addEventListener('click', (e) => {
-  UI.deleteBook(e.target.parentElement.id);
-  Store.removeBook(e.target.parentElement.id);
-});
-
 UI.displayBook();
